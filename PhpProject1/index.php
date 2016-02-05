15806f496b965e1c139a2b5e4ad8ccf1e92ca3ee
Igor Santos <igorsantos07@gmail.com>
2016-02-05 01:30:46 -0200

    corrigindo a porra da indentação

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "tentando de novo";
+            echo "tentando de novo";
         ?>
     </body>
 </html>
